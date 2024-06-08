7c31cf7851c5738d91d84fb9d5c68eee2b9068a1
Eason Lau <easonlaulearningios@gmail.com>
2024-06-08 12:21:29 +0800

    eslint init

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  {
+    rules: {
+      semi: ["error", "always"],
+      indent: ["error", 2],
+    }
+  }
+];
+
