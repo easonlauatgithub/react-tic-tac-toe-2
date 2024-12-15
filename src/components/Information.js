8bf7a6244c2482f0fc39e087772cbade82825965
Eason Lau <easonlaulearningios@gmail.com>
2024-12-15 16:04:16 +0800

    information ok

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,7 +1,22 @@
-const Information = () => {
-    return (
-        <div className="information">Informations</div>
-    );
+const Information = ({
+    playerId
+    ,isGameEndedInTie
+    ,hasWinner
+    ,winnerId
+}) => {
+    if(hasWinner){
+        return (
+            <div className="information">Winner is {winnerId}</div>
+        );
+    }else if(isGameEndedInTie){
+        return (
+            <div className="information">Game ended in tie</div>
+        );
+    }else{
+        return (
+            <div className="information">Waiting for {playerId}</div>
+        );
+    }    
   };
   
   export default Information;
